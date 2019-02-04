6fdef69207e353385923b5180ac77b0e75e421da
Jan Ruusuvuori <jan.ruusuvuori@kodan.fi>
2019-02-04 15:14:12 +0200

    describe project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject yleisradio/http-kit-newrelic "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "New Relic instrumentation for HTTP Kit client"
+  :url "https://github.com/Yleisradio/http-kit-newrelic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
