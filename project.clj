f1cf8efb808f9f247ed1074c5703317561390c0c
Lauri Oherd <lauri.oherd@gmail.com>
2021-12-03 08:56:34 +0200

    update dependencies (#3)

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/Yleisradio/http-kit-newrelic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.newrelic.agent.java/newrelic-api "4.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.newrelic.agent.java/newrelic-api "7.4.0"]
                  [robert/hooke "1.3.0"]]
   :signing {:gpg-key "D9A17928"})
